e06dc8db676e8766156293e64d50098d4a4b89a8
Yeung Yiu Hung <hkclex@gmail.com>
2019-01-12 16:38:31 +0800

    Bind locale

--- app/middleware/localization.middleware.ts
+++ app/middleware/localization.middleware.ts
@@ -4,12 +4,14 @@ import { LocalizedMessage } from '../locale/interface';
 import { LocalizableRequest } from '../common/localizable.request.interface';
 import { BaseMiddleware } from 'inversify-express-utils';
 import { injectable } from 'inversify';
+import TYPES from '../constant/types';
 
 @injectable()
 export class LocalizationMiddleware extends BaseMiddleware {
   handler(req: Request, res: Response, next: NextFunction): void {
     const locale: Localization = Localization.shared(this.defaultLocale);
     const messageStore: LocalizedMessage = locale.of(req.acceptsLanguages());
+    this.bind<LocalizedMessage>(TYPES.LocalizedMessage).toConstantValue(messageStore);
     (req as LocalizableRequest).messageStore = messageStore;
     next();
   }
